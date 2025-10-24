c448eefa7c07e499c5c11704398e3f5263eb4817
Camila <contatocamilamenezes@gmail.com>
2025-10-24 17:27:59 -0300

    inicio estudos c p2

---
+++ estudos/estudosp2/estudocompleto.c
@@ -0,0 +1,35 @@
+// * Esse documento conterá as matérias para a prova 2 de AED. 
+
+// TODO - Caso deseje utilizar algum dos códigos, execute na main
+
+// ! Matérias:
+// ? (Códigos de ordenação) 
+/*
+- Selection - ok
+- Bubble - ok
+- Insertion - ok
+- Heap - ok
+- Quick - ok
+- Merge - ok
+- Shell - ok
+- Counting - ok
+*/
+
+// ? (Estruturas de dados flexíveis)
+/*
+- Pilha flex - ok
+- Fila flex - ok
+- lista simples flex - ok
+- Lista dupla flex - ok
+- Matriz flex - ok
+- Árvore - ok
+*/
+
+// ? (Extras)
+/*
+- Coleta de lixo - ok
+- Ordenações parciais - ok
+*/
+
+//* Algoritmos de ordenação ========================
+// ! Selection sort 
